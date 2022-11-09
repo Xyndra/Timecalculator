7278c4bca4929d3f52267b31b632dbe9d4eba225
Senpai <pvnt20@gmail.com>
2022-11-09 05:37:30 +0530

    Added Exception

---
+++ Timecalc/test.py
@@ -0,0 +1,5 @@
+import timecalculator
+
+print(timecalculator.daytosec(0))
+print(timecalculator.daytosec(0.1))
+print(timecalculator.daytosec("baka"))
--- Timecalc/timecalculator/__init__.py
+++ Timecalc/timecalculator/__init__.py
@@ -1,20 +1,28 @@
 import time
 
 
+def raiseError(param):
+    if not isinstance(param, int | float):
+        raise ValueError(f"arg={param} is not Valid Type for this function")
+
+
 def sys_time():
     t1 = time.localtime()
     return time.strftime("%a, %d %b %Y, %H:%M:%S", t1)
 
 
 def mintosec(minutes: int | float) -> int | float:
+    raiseError(minutes)
     return minutes * 60
 
 
 def htosec(hours: int | float) -> int | float:
+    raiseError(hours)
     return hours * 60 * 60
 
 
 def daytosec(days: int | float) -> int | float:
+    raiseError(days)
     return days * 24 * 60 * 60
 
 
@@ -23,6 +31,7 @@ def work_week() -> int | float:
 
 
 def weektosec(weeks: int | float) -> int | float:
+    raiseError(weeks)
     return weeks * 7 * 24 * 60 * 60
 
 
@@ -31,8 +40,10 @@ def weekend() -> int | float:
 
 
 def monthtosec(months: int | float) -> int | float:
+    raiseError(months)
     return months * 30 * 24 * 60 * 60
 
 
 def yeartosec(years: int | float) -> int | float:
+    raiseError(years)
     return years * 365 * 24 * 60 * 60
