c6ac88bcaec4d6dcec1648b6a91119b21b68685c
Praveensenpai <71966071+Praveensenpai@users.noreply.github.com>
2022-11-11 06:28:24 +0530

    function -> classes

--- Timecalc/timecalculator/__init__.py
+++ Timecalc/timecalculator/__init__.py
@@ -1,49 +1,45 @@
 import time
+from dataclasses import dataclass
 
 
-def raiseError(param):
-    if not isinstance(param, int | float):
-        raise ValueError(f"arg={param} is not Valid Type for this function(should be int or float)")
+@dataclass
+class TimeCaluclator:
+    minute = 60
+    hour = 60 * minute
+    day = 24 * hour
+    weekend = 2 * day
+    work_week = 5 * day
+    week = 7 * day
+    month = 30 * day
+    year = 365 * day
 
+    def sys_time(self) -> str:
+        return time.strftime("%a, %d %b %Y, %H:%M:%S", time.localtime())
 
-def sys_time():
-    t1 = time.localtime()
-    return time.strftime("%a, %d %b %Y, %H:%M:%S", t1)
+    def mintosec(self, minutes: float | int) -> float | int:
+        self.raiseError(minutes)
+        return minutes * self.minute
 
+    def htosec(self, hours: float | int) -> float | int:
+        self.raiseError(hours)
+        return hours * self.hour
 
-def mintosec(minutes: int | float) -> int | float:
-    raiseError(minutes)
-    return minutes * 60
+    def daytosec(self, days: float | int) -> float | int:
+        self.raiseError(days)
+        return days * self.day
 
+    def weektosec(self, weeks: float | int) -> float | int:
+        self.raiseError(weeks)
+        return weeks * self.week
 
-def htosec(hours: int | float) -> int | float:
-    raiseError(hours)
-    return hours * 60 * 60
+    def monthtosec(self, months: float | int) -> float | int:
+        self.raiseError(months)
+        return months * self.month
 
+    def yeartosec(self, years: float | int) -> float | int:
+        self.raiseError(years)
+        return years * self.year
 
-def daytosec(days: int | float) -> int | float:
-    raiseError(days)
-    return days * 24 * 60 * 60
-
-
-def work_week() -> int | float:
-    return 1 * 5 * 24 * 60 * 60
-
-
-def weektosec(weeks: int | float) -> int | float:
-    raiseError(weeks)
-    return weeks * 7 * 24 * 60 * 60
-
-
-def weekend() -> int | float:
-    return 2 * 24 * 60 * 60
-
-
-def monthtosec(months: int | float) -> int | float:
-    raiseError(months)
-    return months * 30 * 24 * 60 * 60
-
-
-def yeartosec(years: int | float) -> int | float:
-    raiseError(years)
-    return years * 365 * 24 * 60 * 60
+    def raiseError(self, param) -> None:
+        if not isinstance(param, int | float):
+            raise ValueError(f"arg={param} is not Valid Type for this function(should be int or float)")
